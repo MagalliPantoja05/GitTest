6e9365af41394bd302a8d7cecf28b889edccb42f
Magalli <pantojamagalli05@gmail.com>
2025-02-06 17:34:34 -0600

    Se modificó la organización de las carpetas y se dividio en modulos y clases, se añadió la [Clase 1 de Javascript] y sus respectivos archivos.

---
+++ Javascript/datos.js
@@ -0,0 +1,29 @@
+console.log(typeof 42);
+
+console.log(typeof 'Veinticinco');
+
+console.log(typeof -666);
+
+console.log(typeof true);
+
+console.log(typeof 0);
+
+console.log(typeof '');
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof FALSE);
+
+console.log(typeof 100000000000);
+
+console.log(typeof 5.55);
+
+console.log(typeof 123456789012345678901234567890n);
+
+console.log(typeof Symbol("id"));
+
+console.log(typeof 1+2);
+
+console.log(typeof "06/02/2024");
